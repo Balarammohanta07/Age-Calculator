11ffcc1c8793f1cd67139195b0cfe7949373bbd8
Balarammohanta07 <155468910+Balarammohanta07@users.noreply.github.com>
2024-08-01 21:14:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculateAge() {
+    const dob = document.getElementById('dob').value;
+    if (dob === '') {
+        alert('Please enter your date of birth');
+        return;
+    }
+
+    const dobDate = new Date(dob);
+    const today = new Date();
+
+    let age = today.getFullYear() - dobDate.getFullYear();
+    const monthDifference = today.getMonth() - dobDate.getMonth();
+
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < dobDate.getDate())) {
+        age--;
+    }
+
+    document.getElementById('result').innerHTML = `You are ${age} years old.`;
+}
